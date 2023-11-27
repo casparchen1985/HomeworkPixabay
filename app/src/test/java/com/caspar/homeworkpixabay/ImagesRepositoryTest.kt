c6e48811d28ee5edbdbef8910d60b0923a1ffbf0
CasparChen <casparchen1985@gmail.com>
2023-11-27 16:02:55 +0800

    Add simple unit test for ImagesRepository

---
+++ app/src/test/java/com/caspar/homeworkpixabay/ImagesRepositoryTest.kt
@@ -0,0 +1,75 @@
+package com.caspar.homeworkpixabay
+
+import com.caspar.homeworkpixabay.di.HomeworkPixabayApplication
+import com.caspar.homeworkpixabay.model.ImagesRepositoryImply
+import com.caspar.homeworkpixabay.model.dataClass.Hit
+import com.caspar.homeworkpixabay.model.dataClass.ImageResult
+import com.caspar.homeworkpixabay.model.interfaceDefine.Images
+import io.mockk.coEvery
+import io.mockk.mockk
+import io.mockk.spyk
+import io.mockk.unmockkAll
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.jupiter.api.Assertions
+import retrofit2.Response
+
+class ImagesRepositoryTest {
+    private val previewUrl = "https://tinyurl.com/yppe7uwv"
+    private val imageUrl = "https://tinyurl.com/4fkxnzp8"
+    private val imageData = listOf(
+        Hit(id = 1, type = "photo", previewURL = previewUrl, imageURL = imageUrl, imageHeight = 1500, imageWidth = 991),
+        Hit(id = 2, type = "photo", previewURL = previewUrl, imageURL = imageUrl, imageHeight = 1500, imageWidth = 991),
+        Hit(id = 3, type = "photo", previewURL = previewUrl, imageURL = imageUrl, imageHeight = 1500, imageWidth = 991),
+        Hit(id = 4, type = "photo", previewURL = previewUrl, imageURL = imageUrl, imageHeight = 1500, imageWidth = 991),
+    )
+    private lateinit var stubService: Images
+
+    @Before
+    fun setup() {
+        val stubApp = spyk(HomeworkPixabayApplication)
+        stubApp.appContext = mockk(relaxed = true)
+        stubService = mockk<Images>()
+    }
+
+    @Test
+    fun testFetchPhotos_normal() = runTest {
+        // set Remote
+        val stubResponse = Response.success(ImageResult(1, 100, imageData))
+        coEvery { stubService.fetchImages(any(), any()) } returns stubResponse
+
+        // set Local
+        val imagesRepos = spyk(ImagesRepositoryImply(stubService))
+
+        // run Test
+        val result = imagesRepos.fetchPhotos("normal", "normal")
+
+        // verify
+        Assertions.assertNotNull(result)
+        Assertions.assertEquals(imageData, result)
+    }
+
+    @Test
+    fun testFetchPhotos_offline() = runTest {
+        // set Remote
+        val stubResponse = Response.error<ImageResult>(444, mockk(relaxed = true))
+        coEvery { stubService.fetchImages(any(), any()) } returns stubResponse
+
+        // set Local
+        val imageRepos = spyk(ImagesRepositoryImply(stubService))
+
+        // run Test
+        val result = imageRepos.fetchPhotos("error", "error")
+
+        // verify
+        Assertions.assertNull(result)
+    }
+
+
+    @After
+    fun tearDown() {
+        unmockkAll()
+    }
+}
