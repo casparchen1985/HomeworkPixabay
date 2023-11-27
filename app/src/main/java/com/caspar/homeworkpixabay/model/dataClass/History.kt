f96ecee135287cf008cd2340fde9cefc2a5ee788
CasparChen <casparchen1985@gmail.com>
2023-11-27 22:41:47 +0800

    Remove useless annotation "Parcelable" on History data class

--- app/src/main/java/com/caspar/homeworkpixabay/model/dataClass/History.kt
+++ app/src/main/java/com/caspar/homeworkpixabay/model/dataClass/History.kt
@@ -1,15 +1,12 @@
 package com.caspar.homeworkpixabay.model.dataClass
 
-import android.os.Parcelable
 import com.caspar.homeworkpixabay.model.interfaceDefine.AsDBObject
 import com.caspar.homeworkpixabay.model.realmObject.HistoryObject
-import kotlinx.parcelize.Parcelize
 
-@Parcelize
 data class History(
     val unix: Long = 0L,
     val keyword: String? = null,
-) : Parcelable, AsDBObject<HistoryObject> {
+) : AsDBObject<HistoryObject> {
     override fun toObject(): HistoryObject {
         return HistoryObject(
             unix, keyword
